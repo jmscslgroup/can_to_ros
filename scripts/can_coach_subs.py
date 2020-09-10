0526ef18103b238cf4f1a9f728eebf785d9b66b2
MatthewNice <mnice@tulane.edu>
2020-09-09 21:32:24 -0500

    switching from pydub to playsound for audio. adding relv into canCoach.

--- scripts/can_coach_subs.py
+++ scripts/can_coach_subs.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import sys
-sys.path.append('~/anaconda3/lib/python3.7/site-packages/panda/')
+#sys.path.append('~/anaconda3/lib/python3.7/site-packages/panda/')
 #from panda import Panda
 import binascii
 #import bitstring
@@ -33,6 +33,8 @@ from pydub.playback import play
 from pydub.generators import Sine, Sawtooth, WhiteNoise, Pulse, Square
 import threading, time, random
 
+from playsound import playsound
+
 
 startTime = time.time() #reference start time
 validTime = time.time()
@@ -44,8 +46,8 @@ high = Sine(600)
 slow = low.to_audio_segment(duration=200)
 fast = high.to_audio_segment(duration=200)
 
-play(slow)
-play(fast)
+#play(slow)
+#play(fast)
 
 df = 0.04
 leader = kt.KF_Object([5,0],0, 0, df, 1,1,1)
@@ -63,33 +65,42 @@ vmax = 100 #maximum volume
 lastBeep = time.time()
 start = time.time()
 
+#def testToSound():
+	#this function gets input from the simulink model on the current 
+fast = '/home/eternity/fastSound.wav' #add a relative path so it works for others
+slow = '/home/eternity/slowSound.wav' #add a relative path so it works for others
+
 def printit():
-	t = threading.Timer(0.5, printit)
-	t.start()
+	###This is the function that gives the sound feedback to the driver. It need to know the 'mode', or test that the driver is in.
+	###It subscribes to both the mode, and keeps track of the current mode.
+		
+	
+	
 	# print(time.time())
     # print('anything')
-	print(th)
+	print(th, lead, relv)
 	if th > 2.1:
 		rospy.loginfo("faster")
-		play(fast)
+		playsound(fast)
 		#if th > 2.2:
 		#play_obj = sa.play_buffer(faster,1,2,44100) #two higher beeps indicating speed up
 	if th < 2.1 and th > 2.05:
 		rospy.loginfo("fast")
-		play(fast)
+		playsound(fast)
 		#play_obj = sa.play_buffer(fast,1,2,44100)
 	if th < 1.9 and th != -1:
 		# if th < 1.8:
-		play(slow)
+		playsound(slow)
 		rospy.loginfo("slower")
         #play_obj = sa.play_buffer(slower,1,2,44100)
 	if th > 1.9 and th < 1.95:
-		play(slow)
+		playsound(slow)
         #play_obj = sa.play_buffer(slow,1,2,44100)
 		rospy.loginfo("slow")
-
-	def cancel():
-		t.cancel()
+	t = threading.Timer(0.5, printit)
+	t.start()
+	
+	return t
 
 def sound():
 	print(time.ctime())
@@ -102,8 +113,6 @@ def sound():
 	threading.Timer(interval,sound).start()
 
 interval = .5
-
-	
 
 #ticker = threading.Event()
 
@@ -126,69 +135,70 @@ def callbackvel(data):
 
 def callback384(data):
     global gmyDetections
+    #print('a1 callback')
     gmyDetections.append([data.pose.position.x, data.pose.position.y])
     #np.append(gmyDetections,[[data.pose.position.x, data.pose.position.y]],axis=0) 
     # print ("trackA0")
     # print(data.pose.position.x, data.pose.position.y)
 def callback385(data):
     global gmyDetections
-    gmyDetections.append([data.pose.position.x, data.pose.position.y])
+    gmyDetections.append([data.pose.position.x, data.pose.position.y,data.pose.position.z])
     # np.append(gmyDetections,[[data.pose.position.x, data.pose.position.y]],axis=0) 
 def callback386(data):
-    global gmyDetections
-    gmyDetections.append([data.pose.position.x, data.pose.position.y])
+	global gmyDetections
+	gmyDetections.append([data.pose.position.x, data.pose.position.y,data.pose.position.z])
     # np.append(gmyDetections,[[data.pose.position.x, data.pose.position.y]],axis=0) 
 def callback387(data):
-    global gmyDetections
-    gmyDetections.append([data.pose.position.x, data.pose.position.y])
+	global gmyDetections
+	gmyDetections.append([data.pose.position.x, data.pose.position.y,data.pose.position.z])
     # np.append(gmyDetections,[[data.pose.position.x, data.pose.position.y]],axis=0)
 def callback388(data):
-    global gmyDetections
-    gmyDetections.append([data.pose.position.x, data.pose.position.y])
+	global gmyDetections
+	gmyDetections.append([data.pose.position.x, data.pose.position.y,data.pose.position.z])
     # np.append(gmyDetections,[[data.pose.position.x, data.pose.position.y]],axis=0)
 def callback389(data):
     global gmyDetections
-    gmyDetections.append([data.pose.position.x, data.pose.position.y])
+    gmyDetections.append([data.pose.position.x, data.pose.position.y,data.pose.position.z])
     # np.append(gmyDetections,[[data.pose.position.x, data.pose.position.y]],axis=0)
 def callback390(data):
     global gmyDetections
-    gmyDetections.append([data.pose.position.x, data.pose.position.y])
+    gmyDetections.append([data.pose.position.x, data.pose.position.y,data.pose.position.z])
     # np.append(gmyDetections,[[data.pose.position.x, data.pose.position.y]],axis=0)
 def callback391(data):
     global gmyDetections
-    gmyDetections.append([data.pose.position.x, data.pose.position.y])
+    gmyDetections.append([data.pose.position.x, data.pose.position.y,data.pose.position.z])
     # np.append(gmyDetections,[[data.pose.position.x, data.pose.position.y]],axis=0)
 def callback392(data):
     global gmyDetections
-    gmyDetections.append([data.pose.position.x, data.pose.position.y])
+    gmyDetections.append([data.pose.position.x, data.pose.position.y,data.pose.position.z])
     # np.append(gmyDetections,[[data.pose.position.x, data.pose.position.y]],axis=0)
 def callback393(data):
     global gmyDetections
-    gmyDetections.append([data.pose.position.x, data.pose.position.y])
+    gmyDetections.append([data.pose.position.x, data.pose.position.y,data.pose.position.z])
     # np.append(gmyDetections,[[data.pose.position.x, data.pose.position.y]],axis=0)
 def callback394(data):
     global gmyDetections
-    gmyDetections.append([data.pose.position.x, data.pose.position.y])
+    gmyDetections.append([data.pose.position.x, data.pose.position.y,data.pose.position.z])
     # np.append(gmyDetections,[[data.pose.position.x, data.pose.position.y]],axis=0)
 def callback395(data):
     global gmyDetections
-    gmyDetections.append([data.pose.position.x, data.pose.position.y])
+    gmyDetections.append([data.pose.position.x, data.pose.position.y,data.pose.position.z])
     # np.append(gmyDetections,[[data.pose.position.x, data.pose.position.y]],axis=0)
 def callback396(data):
     global gmyDetections
-    gmyDetections.append([data.pose.position.x, data.pose.position.y])
+    gmyDetections.append([data.pose.position.x, data.pose.position.y,data.pose.position.z])
     # np.append(gmyDetections,[[data.pose.position.x, data.pose.position.y]],axis=0)
 def callback397(data):
     global gmyDetections
-    gmyDetections.append([data.pose.position.x, data.pose.position.y])
+    gmyDetections.append([data.pose.position.x, data.pose.position.y,data.pose.position.z])
     # np.append(gmyDetections,[[data.pose.position.x, data.pose.position.y]],axis=0)
 def callback398(data):
     global gmyDetections
-    gmyDetections.append([data.pose.position.x, data.pose.position.y])
+    gmyDetections.append([data.pose.position.x, data.pose.position.y,data.pose.position.z])
     # np.append(gmyDetections,[[data.pose.position.x, data.pose.position.y]],axis=0)
 def callback399(data):
     global gmyDetections
-    gmyDetections.append([data.pose.position.x, data.pose.position.y])
+    gmyDetections.append([data.pose.position.x, data.pose.position.y,data.pose.position.z])
     # np.append(gmyDetections,[[data.pose.position.x, data.pose.position.y]],axis=0)
 
 leadDist = 20
@@ -207,7 +217,6 @@ relv = -50
 accel = -50
 leadPosition=[3,0]
 lead = 0
-
 
 
 
@@ -236,65 +245,43 @@ try:
 
 	r = rospy.Rate(50)
 	# sound()
-	printit()
+	t = printit()
 	while not rospy.is_shutdown():
         # print('starting while loop')
 		velocity = gnewVel
 		print(velocity)
 		while not rospy.is_shutdown():
 			
-			# while not ticker.wait(interval):
-				# sound()
-				# print(velocity,th)
-			# print(velocity)
 			now = time.time()
-			# if now > reportTime:
-			# reportTime = time.time()+df
-			# counter += 1
-			# print(counter)
-			# leadArray.append(leader.get_coords()[1]) #KF of clustered radar measurement
-			# vArray.append(velocity)#most recent velocity measurement converted to m/s
-			# aArray.append(accel)
-			# relvArray.append(myrelv) #do NOT convert to kph
+
 			if velocity > 0:# and (now-validTime < 2):
 				th = (leader.get_coords()[0])/(velocity) #distance divided by meters per second
 				# print(th)
 			else:
 				th = -1
-			# thArray.append(th)
-			# thTime.append(now)
-			# twos.append(2)
+
 
 			leader.predict()
-			
+			#print(gmyDetections)
 			if gnewLeadMeasurement != None:
 				
-				# print(len(gmyDetections))
-				# print('starting cluster if')
 				#leadPosition,relv,newVelocity,newAccel = lt.getClusteredRadar(writer = None,recentLeadMeasurement = leadDist,velNaccel= True,gnewLeadMeasurement = gnewLeadMeasurement, gmyDetections = gmyDetections) #0.03 second timeout
 
-				# print(myDetections)
 				#cluster 869 and radar points and choose one radar point
 				if len(gmyDetections) > 0: #and leadMeasurement != None: # if there are new radar measurements
-					# print("inside3 if")
+
 					#radarPlus = np.concatenate((gmyDetections,[[0,gnewLeadMeasurement]]) )#radar measurements plus 869 measurement
-					#if len(radarPlus) > 1:
-					# print('pre-clustered')
-					# print([gnewLeadMeasurement,0],gmyDetections)
+
 					labels = lt.clusterRadar([gnewLeadMeasurement,0],gmyDetections) #cluster algorithm gives labels
-					# print('clustered')
-					#i869 = labels[-1] #the cluster the 869 measurement is in
-					#indices = [i for i, x in enumerate(labels) if x == i869]#find relevant indices of good cluster points
-					#indices = np.where(labels == i869)[0] #this should be faster
 
 					myPoints = labels #indices[:-1] #take out 869 from the key points
 					# print(myPoints,indices)
 					if len(myPoints) > 0: #if there is a point other than from 869 in cluster
 						iLead = random.choice(myPoints)#just choose one randomly
-						lead = gmyDetections[iLead] #lead coordinates
-						# relv = myRelv[iLead] #lead relv
-						# print ("lead", lead)
-						# print(lead,relv)
+						lead = gmyDetections[iLead][0:2] #lead coordinates
+						relv = gmyDetections[iLead][2]
+						
+						#print(lead,relv)
 						gnewLeadMeasurement = None
 						gmyDetections = []#np.empty_like([[0,0]])
 						leader.update(lead) #update kf
@@ -314,18 +301,18 @@ try:
 					# print(myPoints,indices)
 					if len(myPoints) > 0: #if there is a point other than from 869 in cluster
 						iLead = random.choice(myPoints)#just choose one randomly
-						lead = gmyDetections[iLead] #lead coordinates
-						# relv = myRelv[iLead] #lead relv
+						lead = gmyDetections[iLead][0:2] #lead coordinates
+						relv = gmyDetections[iLead][2] #lead relv
 						# print ("lead2", lead)
-						# print(lead,relv)
+						#print(lead,relv)
 						# gnewLeadMeasurement = None
-						gmyDetections = []#np.empty_like([[0,0]])
+						#gmyDetections = []#np.empty_like([[0,0]])
 						leader.update(lead) #update kf
         #this controller operates under the assumption that there is a lead vehicle
 			velocity = gnewVel
                 
 		print('before sleep')
-		printit.cancel()
+		t.cancel()
 		r.sleep()
 	print('before spin')
 except ValueError:
