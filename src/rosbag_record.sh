8e56b7d704322f7d3a3271bef8402aeac9bd6c3d
Rahul Bhadani <rahulbhadani@email.arizona.edu>
2021-07-17 13:03:59 -0700

    conditional checking of second argument for rosbag record

--- src/rosbag_record.sh
+++ src/rosbag_record.sh
@@ -11,9 +11,19 @@ TIME=$(date +%Y_%m_%d_%H_%M_%S)
 
 FILENAME="${TIME}_${VIN}"
 
-DIR_PATH="/var/panda/CyverseData/JmscslgroupData/bagfiles/${DATE}/"
+# If true is supplied then we are running HWIL on PI
+if [[ $2 == 'true' ]]
+then
+	DIR_PATH="/var/panda/CyverseData/JmscslgroupData/bagfiles/${DATE}/"
+fi
 
-if [ $2 == "false" ]
+# if no second argument is supplied, it means we are runnong HWIL on PI 
+if [[ -z "$2" ]]
+then
+	DIR_PATH="/var/panda/CyverseData/JmscslgroupData/bagfiles/${DATE}/"
+fi
+
+if [[ $2 == "false" ]]
 then
         hs=`hostname`
 	DIR_PATH="/home/${hs}/.ros/${DATE}/"
