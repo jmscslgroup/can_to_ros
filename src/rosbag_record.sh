047232d9c8f02e7832d7cd48d26e087d8da847c2
Rahul Bhadani <rahulbhadani@email.arizona.edu>
2021-07-16 20:56:17 -0700

    Addressed the enhancement #5

--- src/rosbag_record.sh
+++ src/rosbag_record.sh
@@ -13,6 +13,12 @@ FILENAME="${TIME}_${VIN}"
 
 DIR_PATH="/var/panda/CyverseData/JmscslgroupData/bagfiles/${DATE}/"
 
+if [ $2 == "false" ]
+then
+        hs=`hostname`
+	DIR_PATH="/home/${hs}/.ros/${DATE}/"
+fi
+
 mkdir -p ${DIR_PATH}
 
 rosbag record -O ${DIR_PATH}${FILENAME}${DESCRIPTION} -a -x /realtime_raw_data
