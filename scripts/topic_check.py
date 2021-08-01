49bff42a18b68c9339fdeed511408753a2e9cc09
safwanelmadani <safwanelmadani@email.arizona.edu>
2021-08-01 12:50:46 -0500

    updating topic_check node

--- scripts/topic_check.py
+++ scripts/topic_check.py
@@ -12,13 +12,27 @@ class TopicCheck:
     def __init__(self):
         rospy.init_node('TopicCheck', anonymous=True)
         # self.pub = rospy.Publisher('/', Float64, queue_size=10)
+        rospy.Subscriber("/car/libpanda/controls_allowed", Bool, self.callback)
         self.rate = rospy.Rate(10)
+        self.timer = threading.Timer(3,self.timeout) # If 3 seconds elapse, call timeout()
+        self.timer.start()
+        rospy.spin()
+        # self.rate = rospy.Rate(10)
 
-    def publisher(self):
 
-        while not rospy.is_shutdown():
-            try:
-                output = subprocess.check_output("rostopic echo -n1 /car/libpanda/controls_allowed", shell=True).decode('utf-8')
+    def timeout(self):
+        print("No message received for 3 seconds")
+        subprocess.check_output("echo -1 > /etc/libpanda.d/controls_allowed", shell=True)
+        # Do something
+
+    def callback(self, msg):
+        # global timer
+        print("Message received")
+        self.timer.cancel()
+        self.timer = threading.Timer(3,self.timeout)
+        self.timer.start()
+
+        output = subprocess.check_output("rostopic echo -n1 /car/libpanda/controls_allowed", shell=True).decode('utf-8')
                 # output = subprocess.check_output("rostopic echo -n1 /test", shell=True).decode('utf-8')
             
                 # print(output)
@@ -32,36 +46,61 @@ class TopicCheck:
 
                 print(data)
 
-                if not data:
-                    subprocess.check_output("echo -1 > /etc/libpanda.d/controls_allowed", shell=True)
-
-                elif (data == "True" ):
+                if (data == "True" ):
                     subprocess.check_output("echo 1 > /etc/libpanda.d/controls_allowed", shell=True)
                 
                 elif (data == "False"):
                     subprocess.check_output("echo 0 > /etc/libpanda.d/controls_allowed", shell=True)
+        # Do Other thing
+
+    # def publisher(self):
+
+    #     while not rospy.is_shutdown():
+    #         try:
+    #             output = subprocess.check_output("rostopic echo -n1 /car/libpanda/controls_allowed", shell=True).decode('utf-8')
+    #             # output = subprocess.check_output("rostopic echo -n1 /test", shell=True).decode('utf-8')
+            
+    #             # print(output)
+    #             var1 = output.replace('data: ', '')
+    #             var1 = var1.replace('---', '')
+    #             # print(var1)
+    #             data = var1.strip()
+
+    #             # while("" in data) : 
+    #             #     data.remove("")
+
+    #             print(data)
+
+    #             if not data:
+    #                 subprocess.check_output("echo -1 > /etc/libpanda.d/controls_allowed", shell=True)
+
+    #             elif (data == "True" ):
+    #                 subprocess.check_output("echo 1 > /etc/libpanda.d/controls_allowed", shell=True)
+                
+    #             elif (data == "False"):
+    #                 subprocess.check_output("echo 0 > /etc/libpanda.d/controls_allowed", shell=True)
 
 
-                # if any(node in output for node in node_list):
-                #     # print('pid is running')
-                #     # print("Working")
-                #     pass  # pid node is running
-                # else:
-                #     # print("something")
-                #     msg = Float64()
-                #     msg.data = 0.0
-                #     # rospy.loginfo(hello_str)
-                #     self.pub.publish(msg)
-                #     self.pub_1.publish(msg)
-                #     self.rate.sleep()
+    #             # if any(node in output for node in node_list):
+    #             #     # print('pid is running')
+    #             #     # print("Working")
+    #             #     pass  # pid node is running
+    #             # else:
+    #             #     # print("something")
+    #             #     msg = Float64()
+    #             #     msg.data = 0.0
+    #             #     # rospy.loginfo(hello_str)
+    #             #     self.pub.publish(msg)
+    #             #     self.pub_1.publish(msg)
+    #             #     self.rate.sleep()
 
-            except:
-                print("topic_check except")
+    #         except:
+    #             print("topic_check except")
 
 if __name__ == '__main__':
     try:
         obj1 = TopicCheck()
-        obj1.publisher()
+        # obj1.publisher()
 
     except:
         print("An exception occurred")
