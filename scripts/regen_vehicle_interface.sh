9dbff61428ce811ac55d454b0a2f9968eba95034
Alex Richardson <william.a.richardson@vanderbilt.edu>
2022-09-26 03:42:51 +0100

    Vehicle regeneration with regards to the vehicle_interface_nissian.cpp file was not working - at least, apparently. This *should* make that script properly run for toyotas AND Nissans now.

--- scripts/regen_vehicle_interface.sh
+++ scripts/regen_vehicle_interface.sh
@@ -7,24 +7,28 @@ start=$((start+1))
 end="$(echo "$a" | sed -n '/msgs to be published end/=' ../src/vehicle_interface_nissan.cpp)"
 end=$((end-1))
 
-cmd="sed -i '' '${start}, ${end} d' ../src/vehicle_interface_nissan.cpp"
-eval $cmd
+if [[ $end -gt $start ]]
+then
+  cmd="sed -i '${start}, ${end} d' ../src/vehicle_interface_nissan.cpp"
+  echo $cmd
+  eval $cmd
+fi
 
 
 ##this section reads in the values from python helper to be published
 
-OUTPUT=($(python insert_lines_nissan_interface.py | tr -d '[],'))
+OUTPUT=($(python3 insert_lines_nissan_interface.py | tr -d '[],'))
 
 ##this section writes the lines to the file
 
 ## for each value pair from python:
 ## sed 'lineNum a\text' fileName
-start=$((start-1))
+#start=$((start-1))
 for i in ${OUTPUT[*]};
 do
   #echo $i;
-  sed -i '' "$start"' a\
-  '"$i
-  " ../src/vehicle_interface_nissan.cpp
+  cmdLoop="sed -i '${start} i ${i}' ../src/vehicle_interface_nissan.cpp"
+  echo $cmdLoop
+  eval $cmdLoop
   start=$((start+1))
 done
